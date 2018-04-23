e5e54b29fdbb5f4e62669746e24ac25b58dafd5f
Natallia Sasinovich <natallia.sasinovich@gmail.com>
2018-04-23 21:59:58 +0300

    set up environment

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use('/', express.static(__dirname + '/'));
+app.use('/src', express.static(__dirname + '/src'));
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/src/index.html');
 });
 
 app.listen(3000, function() {

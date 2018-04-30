27e22cb70017c00b799a15193d79ddab7032a6bd
Natallia Sasinovich <natallia.sasinovich@gmail.com>
2018-04-30 22:09:37 +0300

    add no results component

--- src/app.jsx
+++ src/app.jsx
@@ -4,12 +4,14 @@ import Footer from './components/footer/footer';
 import { FilterOptions } from './constants/app_constants';
 import AssetTile from './components/asset_tile/asset_tile';
 import Button from './components/shared/button/button';
+import NoResults from './components/no_results/no_results';
 
 export default class App extends React.Component {
     render() {
         return (
             <div className="app">
                 <Footer />
+                <NoResults />
 
                 <Button buttonClass='filter-button button-selected'>{FilterOptions.GENRE}</Button>
                 <Button buttonClass='filter-button button-unselected'>{FilterOptions.TITLE}</Button>

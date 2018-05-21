68b2c863ba70479d62e5a4a3e8129c6f3d0132fd
Natallia Sasinovich <natallia_sasinovich@epam.com>
2018-05-21 20:20:25 +0300

    add ability to store app state offline

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunkMiddleware from 'redux-thunk';
-import appReducer from './reducers/app_reducer';
+
+import { store } from './store';
 import App from './app';
-
-const store = createStore(
-    appReducer,
-    applyMiddleware(thunkMiddleware),
-);
-
-window.store = store; // will be removed
 
 const root = document.getElementById('root');
 
